123d3b056c9993f1c5fa4a097528adbe5cb6e09f
HORRYZONE <danishfitri131@gmail.com>
2023-08-16 02:54:00 +0800

    Navigate elevatedBtn to learnFluterPage

--- sampleproject/lib/home_page.dart
+++ sampleproject/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sampleproject/learn_flutter_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(onPressed: (){}, child: const Text("learn flutter")),
+      child:
+          ElevatedButton(onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+              return const LearnFlutterpage();
+            }));
+          }, child: const Text("learn flutter")),
     );
   }
 }
---
+++ sampleproject/lib/learn_flutter_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterpage extends StatefulWidget {
+  const LearnFlutterpage({super.key});
+
+  @override
+  State<LearnFlutterpage> createState() => _LearnFlutterpageState();
+}
+
+class _LearnFlutterpageState extends State<LearnFlutterpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
